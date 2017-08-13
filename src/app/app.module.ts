7bc725f392add49b5ee81dd1f9be5b1215961529
ryneschillinger <ryneschillinger@gmail.com>
2017-08-13 13:41:33 -0700

    working module setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+
+/*---------- COMPONENTS ----------*/
+
+import {NavBarComponent} from './core/nav-bar/nav-bar.component';
+
+export const components = [
+	AppComponent,
+	NavBarComponent
+]
+
+
+/*----------  ROUTES  ----------*/
+
+const appRoutes: Routes = [
+
+];
+
+
+/*----------  MODULES  ----------*/
+
+const modules = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  HttpModule,
+  RouterModule.forRoot(appRoutes, { useHash: true }),
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    ...components,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [...modules],
   providers: [],
   bootstrap: [AppComponent]
 })
