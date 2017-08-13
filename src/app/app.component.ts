271a757c66e29aaddf9eb13ae54a2bf566b454a0
ryneschillinger <ryneschillinger@gmail.com>
2017-08-13 15:44:07 -0700

    added navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+  	'./app.component.css',
+    '../styles/normalize.css',
+  	'../styles/church_global-styles.scss',
+  	'../styles/church_vars.scss',
+  	'../styles/church_fonts.scss',
+  ],
+  encapsulation: ViewEncapsulation.None 
 })
 export class AppComponent {
   title = 'app';
--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -3,9 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
+  styleUrls: ['nav-bar.component.scss'],
 })
 export class NavBarComponent {
 
-	message = "holla";
+	links = [
+		{
+			name: "Salon Info",
+			pageAnchor: "info",
+			highlighted: true,
+		},
+		{
+			name: "Services",
+			pageAnchor: "services",
+			highlighted: false,
+		},
+		{
+			name: "The Stylist",
+			pageAnchor: "stylist",
+			highlighted: false,
+		},
+		{
+			name: "Policies",
+			pageAnchor: "policies",
+			highlighted: false,
+		},
+	]
 
 }
