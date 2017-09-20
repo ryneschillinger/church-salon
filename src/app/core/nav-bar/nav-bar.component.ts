a57bbeca4c58be24549745eb183a0fb76ee2c1f2
ryneschillinger <ryneschillinger@gmail.com>
2017-09-20 16:52:26 -0700

    disable scroll while mobile menu open

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ServicesComponent } from '../../sections/services/services.component';
+declare var $:any;
 
 @Component({
   selector: 'nav-bar',
@@ -33,7 +34,36 @@ export class NavBarComponent {
 		},
 	]
 
+	disableScroll() {
+		if (this.mobileMenuOpen === true) {
+			let html = $('html'); 
+			let body = $('body'); 
+		    var initWidth = body.outerWidth();
+		    var initHeight = body.outerHeight();
 
+		    var scrollPosition = [
+		        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+		        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+		    ];
+		    html.data('scroll-position', scrollPosition);
+		    html.data('previous-overflow', html.css('overflow'));
+		    html.css('overflow', 'hidden');
+		    window.scrollTo(scrollPosition[0], scrollPosition[1]);   
+
+		    var marginR = body.outerWidth()-initWidth;
+		    var marginB = body.outerHeight()-initHeight; 
+		    body.css({'margin-right': marginR,'margin-bottom': marginB});
+		}
+		else {
+			let html = $('html');
+		    let body = $('body');
+		    html.css('overflow', html.data('previous-overflow'));
+		    let scrollPosition = html.data('scroll-position');
+		    window.scrollTo(scrollPosition[0], scrollPosition[1]);    
+
+		    body.css({'margin-right': 0, 'margin-bottom': 0});
+		}
+	}
 
 
 }
