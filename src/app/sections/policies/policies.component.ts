4e3e1ae5a005cab9af944242e3a819d4aeb46b5f
ryneschillinger <ryneschillinger@gmail.com>
2017-09-24 16:06:53 -0700

    added thumbnail image_src

--- src/app/sections/policies/policies.component.ts
+++ src/app/sections/policies/policies.component.ts
@@ -17,14 +17,14 @@ export class PoliciesComponent {
 		{
 			header: 'RUNNING LATE',
 			paragraphs: [
-				'Sometimes unforeseen circumstances mean running a little tardy to the party. Whether its traffic, a slow moving barista at the coffee shop, or a last minute schedule change, sometimes the universe tries to screw with your day, I’ve been there. If you arrive more than 10 minutes late to your scheduled appointment, I will do everything within my power to accommodate you which may include asking you to reschedule to a different date.',
+				'Sometimes unforeseen circumstances mean running a little tardy to the party. Whether its traffic, a slow moving barista at the coffee shop, or a last minute schedule change, sometimes the universe tries to screw with your day. I’ve been there. If you arrive more than 10 minutes late to your scheduled appointment, I will do everything within my power to accommodate you which may include asking you to reschedule to a different date.',
 			]
 		},
 		{
-			header: 'NO-SHOW POLICY/RESCHEDULING/CANCELLATIONS',
+			header: 'NO-SHOW POLICY / RESCHEDULING / CANCELLATIONS',
 			paragraphs: [
-				'If you need to change or cancel your appointment, I ask for a full 24 hours notice. I have reserved the time for you; however, I would appreciate the opportunity to make someone else look fly in that time, and a full day’s notice helps me do so.  So if you have a 9am on Friday and wanna reschedule please let me know before 9am on Thursday.',
-				'Your appointments are reserved especially for you, but hey, sometimes schedule adjustments are necessary; that’s why I respectfully request a full 24-hour’s notice for cancellations. Less than 24 hour notice may result in a charge equal to 50% of the reserved service amount. And friends, please don’t No-Show me.  I reserve the right to charge 100% of the reserved amount if you don’t show.  These charges must be paid before I will reserve more time on the schedule for you.  So be cool and show up, so I don’t have to make it weird.',
+				'If you need to change or cancel your appointment, I ask for a full 24 hours notice. I have reserved the time for you; however, I would appreciate the opportunity to make someone else look fly in that time, and a full day’s notice helps me do so. So if you have a 9am on Friday and wanna reschedule please let me know before 9am on Thursday.',
+				'Your appointments are reserved especially for you, but hey, sometimes schedule adjustments are necessary. That’s why I respectfully request a full 24-hour’s notice for cancellations. Less than 24 hour notice may result in a charge equal to 50% of the reserved service amount. And friends, please don’t No-Show me.  I reserve the right to charge 100% of the reserved amount if you don’t show. These charges must be paid before I will reserve more time on the schedule for you. So be cool and show up so I don’t have to make it weird.',
 			]
 		},
 		{
