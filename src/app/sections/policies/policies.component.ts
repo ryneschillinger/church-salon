0983362706fda7721d50efbe23d2e68d3333340a
ryneschillinger <ryneschillinger@gmail.com>
2017-09-24 21:06:17 -0700

    fixed internet explorer styling bugs

--- src/app/sections/policies/policies.component.ts
+++ src/app/sections/policies/policies.component.ts
@@ -24,13 +24,13 @@ export class PoliciesComponent {
 			header: 'NO-SHOW POLICY / RESCHEDULING / CANCELLATIONS',
 			paragraphs: [
 				'If you need to change or cancel your appointment, I ask for a full 24 hours notice. I have reserved the time for you; however, I would appreciate the opportunity to make someone else look fly in that time, and a full day’s notice helps me do so. So if you have a 9am on Friday and wanna reschedule please let me know before 9am on Thursday.',
-				'Your appointments are reserved especially for you, but hey, sometimes schedule adjustments are necessary. That’s why I respectfully request a full 24-hour’s notice for cancellations. Less than 24 hour notice may result in a charge equal to 50% of the reserved service amount. And friends, please don’t No-Show me.  I reserve the right to charge 100% of the reserved amount if you don’t show. These charges must be paid before I will reserve more time on the schedule for you. So be cool and show up so I don’t have to make it weird.',
+				'Your appointments are reserved especially for you, but hey, sometimes schedule adjustments are necessary. That’s why I respectfully request a full 24-hour’s notice for cancellations. Less than 24 hour notice may result in a charge equal to 50% of the reserved service amount. And friends, please don’t no-show me.  I reserve the right to charge 100% of the reserved amount if you don’t show. These charges must be paid before I will reserve more time on the schedule for you. So be cool and show up so I don’t have to make it weird.',
 			]
 		},
 		{
 			header: 'DEPOSITS',
 			paragraphs: [
-				'Cases like color corrections may require a non-refundable deposit of 50% of the estimated cost of the service when you book the appointment.  The deposit will be credited toward your final total.',
+				'Cases like color corrections may require a non-refundable deposit of 50% of the estimated cost of the service when you book the appointment. The deposit will be credited toward your final total.',
 			]
 		},
 	]
